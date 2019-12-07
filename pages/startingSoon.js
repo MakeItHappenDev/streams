cd681dd6f8aadb50ec20a5e474273f17dc1f0df3
PookMook <arthur.juchereau@gmail.com>
2019-12-07 12:45:39 -0500

    letter animation

--- pages/startingSoon.js
+++ pages/startingSoon.js
@@ -4,28 +4,42 @@ import styles from './startingSoon.module.scss'
 
 export default () => {
 
-     const [show,setShow] = useState(true)
-
-    useEffect(()=>{
-        if(show){
-            //title is showed, timeout 10s to clear it
-            setTimeout(()=>setShow(false),10000)
+     const [step,setStep] = useState(0)
+     const stepUp = () => {
+         console.log("stepup ",step)
+        if(step == 2){
+            setStep(0)
         }
         else{
-            //title is cleared, timeout 5s to show it
-            setTimeout(()=>setShow(true),5000)
+            setStep(step+1)
         }
-    },[show])
+     }
+     
+
+    useEffect(()=>{
+        if(step === 0){
+            //title is steped, timeout 10s to clear it
+            setTimeout(()=>stepUp(),1000)
+        }
+        else if(step == 1){
+            setTimeout(()=>stepUp(),10000)
+        }
+        else{
+            //title is cleared, timeout 5s to step it
+            setTimeout(()=>stepUp(),4000)
+        }
+    },[step])
 
 
     return (
         <main className={styles.container}>
             <section className={styles.title}>
-                <h1 className={show?styles.show:null}>
-                    <span>Starting</span>
-                    <span>soon</span>
+                <h1 className={step == 1?styles.show:step == 2?styles.hide:null}>
+                    <span>{'Starting'.split('').map((c,i)=><span key={i}>{c}</span>)}</span>
+                    <span>{'soon'.split('').map((c,i)=><span key={i}>{c}</span>)}</span>
                 </h1>
             </section>
+            <nav></nav>
         </main>
     )
 }
