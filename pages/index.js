daa4d195ed4ec9999e96ed4915f11093aceebb28
PookMook <arthur.juchereau@gmail.com>
2020-01-04 20:52:12 -0500

    change color scheme

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ const Home = () => {
     <div>
       <p><Link ><a href="/scenes/startingSoon">Starting soon</a></Link></p>
       <p><Link ><a href="/scenes/beRightBack">Be Right Back</a></Link></p>
+      <p><Link ><a href="/scenes/streamEnded">Stream Ended</a></Link></p>
+      <p><Link ><a href="/scenes/justChat">Just Chat</a></Link></p>
     </div>
   )
 }
